1c98af711c791c1512f920204fc1b10c391de8b7
Arakawa Ryu <104044024+ryu-ua@users.noreply.github.com>
2024-05-31 13:26:03 +0900

    復讐問題

--- fukuv0531/Form1.cs
+++ fukuv0531/Form1.cs
@@ -2,9 +2,32 @@ namespace fukuv0531
 {
     public partial class Form1 : Form
     {
+        int vy = 15;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            vy = -15;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            vy = 15;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Top = label1.Top + vy;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "ç≤ãvä‘ óIê∂";
+            vy = 0;
+            timer1.Enabled = false;
         }
     }
 }
